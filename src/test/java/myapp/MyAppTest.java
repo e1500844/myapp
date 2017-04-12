b56a1ba449149ee421cacff580eb25dcf9ade094
e1600538 <quentin.flueckiger@hispeed.ch>
2017-04-12 15:30:25 +0300

    travis

--- src/test/java/myapp/MyAppTest.java
+++ src/test/java/myapp/MyAppTest.java
@@ -2,6 +2,14 @@ package myapp;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -36,6 +44,50 @@ public class MyAppTest {
     
     @Test
     public void test(){
-    	assertEquals(2,2);
+
+  	  try {
+
+  		DefaultHttpClient httpClient = new DefaultHttpClient();
+  		HttpGet getRequest = new HttpGet(
+  			"http://localhost:8080/test");
+  		getRequest.addHeader("accept", "application/json");
+
+  		HttpResponse response = httpClient.execute(getRequest);
+
+  		if (response.getStatusLine().getStatusCode() != 200) {
+  			throw new RuntimeException("Failed : HTTP error code : "
+  			   + response.getStatusLine().getStatusCode());
+  		}
+
+  		BufferedReader br = new BufferedReader(
+                           new InputStreamReader((response.getEntity().getContent())));
+
+    	String excepted = "{\"id\":123}";
+  		
+  		String output = br.readLine();
+  		System.out.println("Output from Server: " + output);
+  		
+  		assertEquals(excepted, output);
+
+  		httpClient.getConnectionManager().shutdown();
+
+  	  } catch (ClientProtocolException e) {
+
+  		e.printStackTrace();
+
+  	  } catch (IOException e) {
+
+  		e.printStackTrace();
+  	  }
+
+  	}
+    
+    @Test
+    public void test2(){
+    	MyController mc = new MyController();
+    	String shouldbe = mc.test("");
+    	assertEquals(shouldbe, "{\"id\":123}");
+    	System.out.println("************ " + shouldbe + " ****************");
     }
 }
+
